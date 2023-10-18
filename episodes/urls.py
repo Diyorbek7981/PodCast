129e10cbc14d781e72354eb4ad07340ac7e23b79
normurodovwork <normurodovwork@gmail.com>
2023-10-18 21:11:34 +0500

    episodes.html bilan ishladik uga vazifa episode.html tugatish

--- episodes/urls.py
+++ episodes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
-    path('',EpisodesView.as_view(),name='episodes'),
+    path('', EpisodesView.as_view(), name='episodes'),
+    path('<int:pk>', EpisodeView.as_view(), name='episode'),
 ]
--- episodes/views.py
+++ episodes/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+
+from episodes.models import Podcast, Category, Tags
 
 
 # Create your views here.
 
-class EpisodesView(TemplateView):
+class EpisodesView(ListView):
+    template_name = 'episodes.html'
+    model = Podcast
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['tags'] = Tags.objects.all()
+        return context
+
+
+class EpisodeView(DetailView):
     template_name = 'episode.html'
+    model = Podcast
+    context_object_name = 'podcast'
